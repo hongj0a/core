26e95fdfb763397dc05f6c41c1c0ba47b13e6bc6
hongj0a <hongj0a@naver.com>
2021-08-11 17:02:11 +0900

    change spring
    change spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //method명에 드러난 명확한 역할 분리, 중복되거나 변경될 객체를 현재 페이지에서 호출할 수 있게 변경함
     //역할과 구현클래스가 한 눈에 들어온다. 애플리케이션 전체 구성을 빠르게 파악할 수 있음
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
