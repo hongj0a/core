e5f1dba1a41e479e598710b27ca592a1f6de9a27
hongj0a <hongj0a@naver.com>
2021-08-11 12:22:46 +0900

    refactoring
    AppConfig.java refactoring
    un use to remove annotation

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,31 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    //method명에 드러난 명확한 역할 분리, 중복되거나 변경될 객체를 현재 페이지에서 호출할 수 있게 변경함
+    //역할과 구현클래스가 한 눈에 들어온다. 애플리케이션 전체 구성을 빠르게 파악할 수 있음
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
