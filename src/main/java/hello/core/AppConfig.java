c64d68bb4c9796a60edf3a2bb7b5f65b6c196441
hongj0a <hongj0a@naver.com>
2021-08-20 16:27:22 +0900

    @Configuration 동작원리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,20 +14,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+
     //method명에 드러난 명확한 역할 분리, 중복되거나 변경될 객체를 현재 페이지에서 호출할 수 있게 변경함
     //역할과 구현클래스가 한 눈에 들어온다. 애플리케이션 전체 구성을 빠르게 파악할 수 있음
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
