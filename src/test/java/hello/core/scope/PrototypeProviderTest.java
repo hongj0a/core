549f3c211c24c99eb9ed3854d710836a37c6b961
hongj0a <hongj0a@naver.com>
2021-08-23 17:14:23 +0900

    빈 스코프
    1. 싱글톤, 프로토타입 차이
    2. 웹 스코프 - request 스코프 예제

---
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,75 @@
+package hello.core.scope;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+    static class ClientBean{
+
+      /* 1.  @Autowired
+        private ApplicationContext ac;*/
+
+      /* 2.  @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;*/
+
+        @Autowired
+        private Provider<PrototypeBean> provider;
+
+        public int logic() {
+            //1. PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            //2. PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+
+            PrototypeBean prototypeBean = provider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
